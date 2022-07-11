9cb60fe844bfedbb168bdef928dc58b304d3754c
Katerina Zhadko <katerinkaj@icloud.com>
2022-07-11 17:55:44 +0300

    feat: add media 390

---
+++ travel/index.js
@@ -0,0 +1,22 @@
+window.addEventListener('resize', function () {
+    activeSliderChange()
+});
+
+window.addEventListener('load', function () {
+    activeSliderChange()
+});
+
+function activeSliderChange() {
+    if (window.innerWidth <= 390) {
+        let firstSlide = document.querySelector('.slider p:first-child');
+        let middleSlide = document.querySelector('.slider p:nth-child(2)');
+        firstSlide.classList.add('active');
+        middleSlide.classList.remove('active')
+    }
+    if (window.innerWidth > 390) {
+        let firstSlide = document.querySelector('.slider p:first-child');
+        let middleSlide = document.querySelector('.slider p:nth-child(2)');
+        middleSlide.classList.add('active');
+        firstSlide.classList.remove('active')
+    }
+}
